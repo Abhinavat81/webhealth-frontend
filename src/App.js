395b36d6cc4d29d4ad2f2540217bd650d9f1a131
Abhinav Thakur <abhinavat81@gmail.com>
2023-12-16 17:03:31 +0530

    File folder structure created

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import "./App.css";
+import React from "react";
+
+// import Header from "./components/header/Header";
+// import Learningtab from "./screen/learningtab/learningtab";
+// import AdminTable from "./components/trainingTab/AdminTable";
+// import Form from "./components/loginPageUI/loginPage";
+// import Report from "./screen/reportTab/report";
+// import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+// import TrainingTabPage from "./screen/trainingTab/TrainingTabPage";
+// import PageNotFoundPage from "./screen/errorPages/PageNotFoundPage";
+// import AccessDeniedPage from "./screen/errorPages/AccessDeniedPage";
+// import InternalServerErrorPage from "./screen/errorPages/InternalServerErrorPage";
+// import CourseContentPage from "./screen/courseContent/CourseContentPage";
+// import ProtectedRoute from "./utils/protectedroutes";
+// import ProtectedRouteAdmin from "./utils/protectedRoutesAdmin";
+
+import { useState, useEffect } from "react";
+// import DashboardPage from "./screen/adminDashboard/dashboard";
+// import Users from "./screen/users/userHome";
+// import UserEditForm from "./screen/users/userEditForm";
+// import ViewUser from "./screen/users/viewUser";
+// // import AddUser from "./components/users/addScreen/addUser";
+// import CreateCourse from "./components/trainingTab/Form";
+
+// import SingleUserReport from "./screen/adminPerformanceReport/SingleUserReport";
+// import PerformanceTable from "./screen/adminPerformanceReport/PerformanceTable";
+// import TotalInternsReport from "./screen/adminPerformanceReport/TotalInternsReport";
+
+// import BatchPage from "./screen/batch/BatchPage";
+// import BatchDetailsPage from "./screen/batch/BatchDetailsPage";
+// import EditBatchPage from "./screen/batch/EditBatchPage";
+
+// import EditCourse from "./components/trainingTab/EditForm";
+// import InfoCourse from "./components/courseContent/CourseContent";
+// import { ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.css";
+// import ConfirmationBox from "./components/trainingTab/ConfirmationBox";
+function App() {
+  // hooks
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+//   // to check auth state
+//   const checkUserToken = () => {
+//     const userToken = localStorage.getItem("token");
+//     if (!userToken || userToken === "undefined") {
+//       setIsLoggedIn(false);
+//     } else {
+//       setIsLoggedIn(true);
+//     }
+//   };
+//   useEffect(() => {
+//     checkUserToken();
+//   }, [isLoggedIn]);
+
+  return (
+    <div className="App p-0 m-0">
+    Abhinav Thakur</div>
+  );
+}
+
+export default App;
